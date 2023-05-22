67e8d81d4a7e16fe5af002954793fd1e77f71ae5
unknown <igniux527@gmail.com>
2023-05-22 19:31:14 +0300

    example

--- pages/api/generateTask.js
+++ pages/api/generateTask.js
@@ -6,16 +6,16 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 export default async function (req, res) {
-  const { concept } = req.body;
+  const { concept, example } = req.body;
   const completion = await openai.createCompletion({
     model: "text-davinci-003",
-    prompt: generatePrompt(concept),
+    prompt: generatePrompt(concept, example),
     temperature: 0,
     max_tokens: 2048,
   });
   res.status(200).json({ result: completion.data.choices[0].text });
 }
 
-function generatePrompt(concept) {
-  return `Generate an task to solve chemistry that is related with ${concept} for high-school students. A bit harder one to solve.`;
+function generatePrompt(concept, example) {
+  return `Generate an task to solve chemistry that is related with ${concept} for high-school students. For example: "${example}" A bit harder one to solve.`;
 }
